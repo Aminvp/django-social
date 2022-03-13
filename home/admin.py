26e240f5025803e20e7d3bf191916e64f36ccbc2
amin <aminvp73@gmail.com>
2022-03-13 15:07:13 +0330

    post like

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Vote
 
 
 @admin.register(Post)
@@ -17,5 +17,5 @@ class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'post', 'reply')
 
 
-
+admin.site.register(Vote)
 
--- home/forms.py
+++ home/forms.py
@@ -16,3 +16,9 @@ class CommentCreateForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
 
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
